5be5b3ead438a713c336ebca81b5414e010da351
Jonathan Cheng <jcheng461@gmail.com>
2023-09-29 10:38:08 -0700

    set up and pass test string field Getters

--- OrderTracker.Tests/ModelTests/VendorTests.cs
+++ OrderTracker.Tests/ModelTests/VendorTests.cs
@@ -16,5 +16,19 @@ namespace OrderTracker.Tests
       Vendor justAddedInstance = new Vendor(inputtedVendorName, inputtedVendorDescription);
       Assert.AreEqual(typeof(Vendor), justAddedInstance.GetType());
     }
+
+    [TestMethod]
+    public void GetStringFields_ReturnsNameAndDescriptionSeparately_String()
+    {
+      string inputtedVendorName = "Sloppy Joe's Extraordinary Peanut Butter Sandwiches";
+      string inputtedVendorDescription = "A local sandwich shop revitalizing the time-tested peanut butter sandwich.";
+      Vendor justAddedInstance = new Vendor(inputtedVendorName, inputtedVendorDescription);
+      // Results of Using Getter
+      string vendorNameUsingGetter = justAddedInstance.Name;
+      string vendorDescUsingGetter = justAddedInstance.Description;
+      // Assert
+      Assert.AreEqual(inputtedVendorName, vendorNameUsingGetter);
+      Assert.AreEqual(inputtedVendorDescription, vendorDescUsingGetter);
+    }
   }
 }
