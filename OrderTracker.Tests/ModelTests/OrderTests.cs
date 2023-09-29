c2f8577c6a8acfb283f18ec972bd728c11bcf1de
Jonathan Cheng <jcheng461@gmail.com>
2023-09-29 08:47:08 -0700

    set up file directory; boilerplate code

---
+++ OrderTracker.Tests/ModelTests/OrderTests.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using OrderTracker.Models;
+using System;
+
+namespace OrderTracker.Tests
+{
+
+  [TestClass]
+  public class OrderTests
+  {
+
+  }
+}
