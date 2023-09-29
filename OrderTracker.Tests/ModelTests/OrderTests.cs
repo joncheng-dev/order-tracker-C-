0fae8d94bec47e68804fecf3ddf48df323039e95
Jonathan Cheng <jcheng461@gmail.com>
2023-09-29 09:05:47 -0700

    set up and pass test for Description get

--- OrderTracker.Tests/ModelTests/OrderTests.cs
+++ OrderTracker.Tests/ModelTests/OrderTests.cs
@@ -12,7 +12,7 @@ namespace OrderTracker.Tests
     [TestMethod]
     public void OrderConstructor_CreatesInstanceOfOrder_Order()
     {
-      Order instanceOfOrder = new Order("some title");
+      Order instanceOfOrder = new Order("a title", "a description");
       Assert.AreEqual(typeof(Order), instanceOfOrder.GetType());
     }
 
@@ -20,9 +20,19 @@ namespace OrderTracker.Tests
     public void GetTitle_ReturnsTitle_String()
     {
       string formCollectedTitle = "Saturday Market Order";
-      Order instanceOfOrder = new Order(formCollectedTitle);
+      Order instanceOfOrder = new Order(formCollectedTitle, "a description");
       string resultFromGet = instanceOfOrder.Title;
       Assert.AreEqual(formCollectedTitle, resultFromGet);      
+    }
+
+    [TestMethod]
+    public void GetDescription_ReturnsDescription_String()
+    {
+      string formCollectedTitle = "Saturday Market Order";
+      string formCollectedDescription = "20 loaves of bread; pre-cut";
+      Order instanceOfOrder = new Order(formCollectedTitle, formCollectedDescription);
+      string resultFromGet = instanceOfOrder.Description;
+      Assert.AreEqual(formCollectedDescription, resultFromGet);     
     }
   }
 }
--- OrderTracker/Models/Order.cs
+++ OrderTracker/Models/Order.cs
@@ -5,11 +5,12 @@ namespace OrderTracker.Models
   public class Order
   {
     public string Title { get; }
+    public string Description { get; }
 
-    public Order(string title)
+    public Order(string title, string description)
     {
       Title = title;
-      // Description = description; 
+      Description = description; 
     }
   }
 }
